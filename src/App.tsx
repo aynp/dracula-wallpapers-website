4d6c4a6adb629f670d647c498a5e2e8aefab3595
lekhit <blekhit@gmail.com>
2022-06-30 05:27:35 +0000

    add magic scroll bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./components/Home";
 import Tab from "./components/Tab";
-
 import data from "./walls.json";
 import Nav from "./components/Nav";
-
+import ProgressBar from "react-scroll-progress-bar"; 
 function App() {
   console.log(data);
+
   return (
+    <>
+     <ProgressBar
+     bgcolor="#6272a4"
+     ></ProgressBar>
     <BrowserRouter>
+   
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Tab />} />
       </Routes>
     </BrowserRouter>
+    </>
   );
 }
 
