6ecec7eef1f99beb497207cc9ccd83e56c32bcc5
Aryan <aryan.pathania2003@gmail.com>
2022-10-03 11:37:01 +0530

    ✨ Added footer

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,9 +1,12 @@
+import Footer from './Footer';
+
 export default function Container(props: any) {
   const { children, ...customMeta } = props;
 
   return (
     <div className="bg-dracula-darker">
       <main className="flex flex-col justify-center px-8 ">{children}</main>
+      <Footer />
     </div>
   );
 }
